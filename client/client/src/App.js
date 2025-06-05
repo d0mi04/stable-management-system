76a8c1e7b6952a5353da4bea9fb662fe28f5c748
Dominika Windows <d0mi04@windows.com>
2025-06-05 23:40:24 +0200

    added login page with basic authentication logic

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Login from './pages/Login';
+// później można dodać np. import NotFound from './components/NotFound'; --> coś takiego na przykład na błąd 404
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />} />
+        {/* <Route path="*" element={<NotFound />} /> --> opcjonalna strona 404 */}
       </Routes>
     </BrowserRouter>
   );
