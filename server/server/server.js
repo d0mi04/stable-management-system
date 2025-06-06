4c7e2f906af0ff277ad3dd829cf9541f9443a9c4
Dominika Windows <d0mi04@windows.com>
2025-06-06 12:27:51 +0200

    add REST API controllers and update server.js

--- server/server/server.js
+++ server/server/server.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
+const stallRoutes = require('./routes/stalls');
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -16,6 +18,11 @@ app.get('/', (req, res) => {
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('✅ Connected to MongoDB Atlas');
-    app.listen(process.env.PORT, () => console.log(`🚀 Serwer is running on port ${process.env.PORT}`));
     })
   .catch(err => console.error('❌ MongoDB connection error:', err));
+
+app.use('/stalls', stallRoutes);
+
+// start:
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Serwer is running on port ${PORT}`));
