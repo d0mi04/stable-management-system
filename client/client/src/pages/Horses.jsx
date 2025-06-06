cc6db666f1a1db5d5c8a4b6fdb525dc0e7433d84
Dominika Windows <d0mi04@windows.com>
2025-06-06 21:18:46 +0200

    basic logic for sending Horse forms

--- client/client/src/pages/Horses.jsx
+++ client/client/src/pages/Horses.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Outlet, Link } from 'react-router-dom';
+import HorseForm from "./admin/Horses/HorseForm";
 
 const Horses = () => {
-  return <h2>Horses (Admin only)</h2>;
+  return (
+    <div>
+      <h2>Horses (Admin only)</h2>
+
+      <Link to="add">Add new Horse</Link>
+      <HorseForm />
+
+      <Outlet />
+    </div>
+  );
 };
 
 export default Horses;
