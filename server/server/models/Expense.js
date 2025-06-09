ea2577f6bbc8e5aad6c7e0fbc070ba2fb4850ee3
Dominika Windows <d0mi04@windows.com>
2025-06-09 18:48:37 +0200

    Expenses schema modification

--- server/server/models/Expense.js
+++ server/server/models/Expense.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const expenseSchema = new mongoose.Schema({
     date: { type: Date, required: true }, // 2025-06-01
     type: { type: String, enum: ['expense', 'income'], required: true }, // expense or income
-    category: { type: String, required: true }, // rent, horse food, repair
-    relatedTo: { type: mongoose.Schema.Types.ObjectId, refPath: 'relatedModel' },
-    relatedModel: { type: String, enum: ['Horse', 'Staff']}, // related model mówi do której kolekcji należy obiekt - Horse lub Staff
+    category: { type: String, required: true }, // rent, horse food, repair, maintenance, etc.
+    relatedTo: { type: mongoose.Schema.Types.ObjectId, refPath: 'relatedModel', default: null },
+    relatedModel: { type: String, enum: ['Horse', 'Staff', 'Stable'], default: null}, // related model mówi do której kolekcji należy obiekt - Horse, Staff, Stable
     amount: { type: Number, required: true }, // 2000
     settled: { type: Boolean, default: false }, // czy wydatek jest rozliczony --> można później filtrować
     description: { type: String } // monthly rent - Fantazja
