7914bd540cfe1ebbf7df357964e8bfd5648d3b5b
Dominika Windows <d0mi04@windows.com>
2025-06-11 21:30:21 +0200

    adding googleId property in User schema

--- server/server/models/User.js
+++ server/server/models/User.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     email: {type: String, required: true, unique: true},
     username: {type: String, required: true, unique: true},
-    password: {type: String, required: true },
+    password: {type: String }, // usunęłam required: true, bo do logowania google to pole będzie puste (chyba)
     role: {type: String, enum: ['admin', 'user'], default: 'user'},
     myHorses: [
         { type: mongoose.Schema.Types.ObjectId, ref: 'Horse'}
-    ]
+    ],
+    googleId: { type: String }
 });
 
 module.exports = mongoose.model('User', userSchema);
