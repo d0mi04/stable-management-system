9dbd1d444842d24e91b2537d94ee3d46f6a96b38
jasiuhavingfun <jan.ziarkowski@gmail.com>
2025-06-13 20:58:13 +0200

    Refactor authentication to use email instead of username; implement API-based login and add seed data for initial users and horses

--- client/client/src/pages/Login.jsx
+++ client/client/src/pages/Login.jsx
@@ -3,16 +3,16 @@ import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
 const Login = () => {
-    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
 
     const { login } = useAuth(); // to dokładam pobranie usera
     const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        const user = login(username, password);
+        const user = await login(email, password);
         if (user) {
             if(user.role === 'admin') {
                 navigate('/admin', { replace: true }); // przenosi na dashboard
@@ -20,7 +20,7 @@ const Login = () => {
                 navigate('/user', { replace: true }); // a to przenosi na user home
             }
         } else {
-            setError('☹️ Invalid username or password!')
+            setError('☹️ Invalid email or password!')
         }
     };
 
@@ -30,11 +30,11 @@ const Login = () => {
                 <h2 className="text-3xl font-bold text-center text-indigo-700 mb-6">Stable Manager Login</h2>
                 <form onSubmit={handleSubmit} className="space-y-5">
                     <div>
-                        <label className="block text-gray-700 font-semibold mb-1">Username</label>
+                        <label className="block text-gray-700 font-semibold mb-1">Email</label>
                         <input
-                            type="text"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
                             required
                             className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-indigo-400"
                             autoFocus
